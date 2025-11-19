e786a0de4271ceda23609ea4023ac8bd5746a1bb
vincentVo <volh@smartosc.com>
2025-11-19 18:08:15 +0700

    feat: enrich Rectangle example with chaining and diagnostics

--- CommonProgrammingConcepts/src/main.rs
+++ CommonProgrammingConcepts/src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
     let mut a: [usize; 10] = [5; 10];
-    let mut sum = 0;
-    for x in &a {
-        sum += x;
-    }
-    println!("{sum}");
+    a[0] = 1;
+    println!("{:?}", a);
 }
