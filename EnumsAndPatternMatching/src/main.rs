fb8e169db6d0d6afbadc66936d7fad91458d437f
vincentVo <volh@smartosc.com>
2025-11-20 13:45:06 +0700

    Add User struct and utility helpers

--- EnumsAndPatternMatching/src/main.rs
+++ EnumsAndPatternMatching/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct User {
+    id: u32,
+    name: String,
+    email: String,
+}
+
+fn get_user_email(user: Option<User>) -> Result<String, String> {
+    let Some(user) = user else {
+        return Err("User not found".to_string());
+    };
+
+    Ok(user.email)
+}
+
+fn make_exciting(s: &str) -> String {
+    s.replace(".", "!").replace("?", "‽")
 }
